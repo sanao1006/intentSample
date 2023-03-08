0fa6c667f1a2a89ec7227539a80b679d7e42e4e5
sanao1006 <naosak1006@gmail.com>
2023-03-08 22:20:40 +0900

    modifying view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.SimpleAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        val menulist: MutableList<MutableMap<String, String>> = mutableListOf()
+        var menu = mutableMapOf<String, String>("name" to "ハンバーグ", "price" to "850円")
+        menulist.add(menu)
+        menu = mutableMapOf<String, String>("name" to "から揚げ", "price" to "500円")
+        menulist.add(menu)
+        menu = mutableMapOf<String, String>("name" to "ラーメン", "price" to "700円")
+        menulist.add(menu)
+
+        val from = arrayOf("name", "price")
+        val to = intArrayOf(android.R.id.text1, android.R.id.text2)
+        val adapter = SimpleAdapter(
+            this@MainActivity,
+            menulist,
+            android.R.layout.simple_list_item_2,
+            from,
+            to
+        )
+        lvMenu.adapter = adapter
+
     }
 }
