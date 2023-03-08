7ffe8f0f25fc176fba7b49abcd000395a866ec3d
sanao1006 <naosak1006@gmail.com>
2023-03-08 22:59:37 +0900

    making ListItemClickListener

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
 
@@ -30,5 +33,20 @@ class MainActivity : AppCompatActivity() {
         )
         lvMenu.adapter = adapter
 
+        lvMenu.onItemClickListener = ListItemClickListener()
+
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            val item = parent!!.getItemAtPosition(position) as MutableMap<String, String>
+            val menuName = item["name"]
+            val menuPrice = item["price"]
+            val intent2MenuThanks = Intent(this@MainActivity, MenuThanksActivity::class.java)
+            intent2MenuThanks.putExtra("menuName", menuName)
+            intent2MenuThanks.putExtra("menuPrice", menuPrice)
+            startActivity(intent2MenuThanks)
+
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MenuThanksActivity.kt
+++ app/src/main/java/com/example/myapplication/MenuThanksActivity.kt
@@ -7,5 +7,7 @@ class MenuThanksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_thanks)
+
+
     }
 }
